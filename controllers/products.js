85e3fda635fd267489946a472dcf93ecfc69d8b1
Saul Chipwayambokoma <chipwayambokomas@gmail.com>
2023-03-16 19:44:20 +0200

    Allow the user to specify which fields they want to receive

--- controllers/products.js
+++ controllers/products.js
@@ -7,7 +7,7 @@ const getAllProductsStatic = async (req,res) => {
 }
 
 const getAllProducts = async (req,res) => {
-    const {featured, company, name, sort} = req.query
+    const {featured, company, name, sort, fields} = req.query
     const queryObject = {}
 
     if(featured){
@@ -26,6 +26,10 @@ const getAllProducts = async (req,res) => {
     }
     else{
         result = result.sort('createdAt')
+    }
+    if(fields){
+        const selectFields = fields.split(',').join(' ')
+        result = result.select(selectFields)
     }
     const products = await result
     res.status(200).json({products})
